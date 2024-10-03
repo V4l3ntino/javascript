ea9b0292074d4452615aee59458d5e7f7e0b10fe
V4l3ntino <valentinohontar15@gmail.com>
2024-10-03 20:18:08 +0200

    correcciones

--- ap11/app.js
+++ ap11/app.js
@@ -1,8 +1,18 @@
 // Ejercicio 11. Escribe un programa que genere 100 números del 1 al 20. Debe mostrar la cuenta de los
 // números que al menos han aparecido una vez.
 
-const listaNumerosRandoms = Array.from({ length: 100 }, () => Math.round(Math.random() * 20));
+const listaNumerosRandoms = Array.from({ length: 10 }, () => Math.round(Math.random() * 9)+1);
+let listavacia = Array.from({length: 10}).fill(0)
 
-for(let item of listaNumerosRandoms){
-    console.log(item)
+let nuevolista = new Array(20).fill(0)
+
+for (let item of listaNumerosRandoms){
+    listavacia[item-1]++;
+}
+
+console.log(listaNumerosRandoms)
+count = 1;
+for(let value of listavacia){
+    console.log(`Numero ${count} -> ${value}`)
+    count++;
 }
